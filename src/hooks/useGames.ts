db9795aceb6d8155b665c54b2cda43d7bb62b8f7
ofargally <ofargally@colgate.edu>
2024-07-21 01:26:42 -0400

    Displayed platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,12 +2,20 @@ import { CanceledError } from "axios";
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 
+export interface Platform {
+    id : number;
+    name: string;
+    slug: string;
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: {platform: Platform}[]
   }
-  interface Response {
+
+interface Response {
     count: number;
     results: Game[];
   }
