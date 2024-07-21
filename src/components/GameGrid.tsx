885684792035e2c55fb06ddd50d2d511753857cd
ZEKIMAR-2xO <omar@ozeki.ai>
2024-07-21 02:27:52 -0400

    refactored - duplicated styles removed

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,14 +2,23 @@ import useGames from "../hooks/useGames";
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames();
   const loadGames = () => {
-    return games.map((game) => <GameCard key={game.id} game={game} />);
+    return games.map((game) => (
+      <GameCardContainer>
+        <GameCard key={game.id} game={game} />
+      </GameCardContainer>
+    ));
   };
   const loadSkeletons = () => {
-    return games.map((game) => <GameCardSkeleton key={game.id} />);
+    return games.map((game) => (
+      <GameCardContainer>
+        <GameCardSkeleton key={game.id} />
+      </GameCardContainer>
+    ));
   };
   return (
     <>
