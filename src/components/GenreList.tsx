3c424111d6ff821b3776c11f5af1285af9d4a6a1
ofargally <ofargally@colgate.edu>
2024-07-22 19:15:07 -0400

    added genre filter by passing /genre as a query string parameter to api

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,18 @@
-import { HStack, List, ListItem, Spinner, Image, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import {
+  HStack,
+  List,
+  ListItem,
+  Spinner,
+  Image,
+  Button,
+} from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+const GenreList = ({ onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (isLoading) return <Spinner />;
@@ -16,7 +26,13 @@ const GenreList = () => {
             borderRadius={8}
             src={getCroppedImageUrl(genre.image_background, 600, 400)}
           />
-          <Text fontSize="lg">{genre.name}</Text>
+          <Button
+            onClick={() => onSelectGenre(genre)}
+            fontSize="lg"
+            variant="link"
+          >
+            {genre.name}
+          </Button>
         </HStack>
       </ListItem>
     ));
