074eb972e8ecc5db4b7d142790605e1742d808fc
ZEKIMAR-2xO <omar@ozeki.ai>
2024-07-22 18:47:50 -0400

    added spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,27 @@
+import { HStack, List, ListItem, Spinner, Image, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if (isLoading) return <Spinner />;
+  if (error) return null;
   const loadGenres = () => {
-    return data.map((genre) => <li key={genre.id}>{genre.name}</li>);
+    return data.map((genre) => (
+      <ListItem paddingY="5px" key={genre.id}>
+        <HStack>
+          <Image
+            boxSize="32px"
+            borderRadius={8}
+            src={getCroppedImageUrl(genre.image_background, 600, 400)}
+          />
+          <Text fontSize="lg">{genre.name}</Text>
+        </HStack>
+      </ListItem>
+    ));
   };
-  return <ul>{loadGenres()}</ul>;
+  return <List>{loadGenres()}</List>;
 };
 
 export default GenreList;
