f13fe340e3ff819b089e7e4797b040541ed20657
ofargally <ofargally@colgate.edu>
2024-07-22 20:38:33 -0400

    refactored query object extraction

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,15 +3,13 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../hooks/useGenres";
-import { Platform } from "../hooks/usePlatforms";
+import { GameQuery } from "../App";
 
 interface Props {
-  selectedGenre: Genre | null;
-  selectedPlatform: Platform | null;
+  gameQuery: GameQuery;
 }
-const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
-  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
+const GameGrid = ({ gameQuery }: Props) => {
+  const { data, error, isLoading } = useGames(gameQuery);
   const loadGames = () => {
     return data.map((game) => (
       <GameCardContainer key={game.id}>
