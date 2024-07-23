6076525339e0600b01bd3a08345992f48c61835b
ZEKIMAR-2xO <omar@ozeki.ai>
2024-07-22 23:06:57 -0400

    added static loading

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,12 +1,12 @@
-import useData from "./useData";
-
+//import useData from "./useData";
+import genres from "../data/genres.ts";
 export interface Genre {
-    id : number;
-    name: string;
-    image_background: string;
+  id: number;
+  name: string;
+  image_background: string;
 }
 
-const endpoint = "/genres";
-const useGenres = () => useData<Genre>(endpoint);
+//const endpoint = "/genres";
+const useGenres = () => ({ data: genres, isLoading: false, error: null }); //useData<Genre>(endpoint);
 
 export default useGenres;
